48608f98b2d9a91a5e79724835fa6437e6ed66b6
Rudi Grinberg <rudi.grinberg@gmail.com>
2017-05-30 01:06:29 -0400

    attempt 1

---
+++ hello_world.ml
@@ -0,0 +1,5 @@
+
+external print_hello : unit -> unit = "caml_print_hello"
+
+let () =
+  print_hello ()
---
+++ hw.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <caml/mlvalues.h>
+
+CAMLprim value caml_print_hello(value unit)
+{
+  printf("Hello\n");
+  return Val_unit;
+}
